f1b349986c05643c18f464320593bb5294781730
Himanshu Rajput <hiowner00@gmail.com>
2023-09-19 19:51:02 +0530

    setup and header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Himanshu Rajput</h1>
+        <h5 className="text-light">Fulllstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        
+        <div className="me">
+            <img src={ME} alt="dp" />
+        </div>
+
+        <a href="#contact" className='scroll-down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
