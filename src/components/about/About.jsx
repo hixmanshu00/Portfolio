b5e4266ec07b5d095d947d25773746e17a1df3bd
Himanshu Rajput <hiowner00@gmail.com>
2023-09-29 16:51:27 +0530

    Experience done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 const About = () => {
   return (
     <section id='about'>
-      about
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+          </div>
+
+          <div className="about-content">
+            <div className="about-cards">
+              <article className="about-card">
+                <FaAward className='about-icon'/>
+                <h5>Experience</h5>
+                <small>3+ years</small>
+              </article>
+              <article className="about-card">
+                <FiUsers className='about-icon'/>
+                <h5>Clients</h5>
+                <small>200+ worldwide</small>
+              </article>
+              <article className="about-card">
+                <VscFolderLibrary className='about-icon'/>
+                <h5>Projects</h5>
+                <small>80+ completed</small>
+              </article>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde atque nobis ea fugit non saepe aliquam autem pariatur porro est. Suscipit eos, illum nobis architecto est atque quam vitae ipsam molestiae reiciendis neque. Architecto minima fugiat, molestias adipisci reiciendis cupiditate illo saepe. Consequuntur, velit! Quaerat sed quo nisi id placeat!
+            </p>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
+      </div>
     </section>
   )
 }
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,109 @@
-import React from 'react'
-import './experience.css'
-
+import React from "react";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 const Experience = () => {
   return (
-    <section id='experience'>
-      experience
-    </section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className="container experience-container">
+        <div className="experience-frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience-content">
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+                <div>
+                <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+                </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+                <div>
+                <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+                </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+                <h4>Javascript</h4>
+              <small className="text-light">Experienced</small>
+                </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>Bootstrap</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div><h4>Tailwind</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>ReactJS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience-backend">
+          <h3>Backend Development</h3>
+          <div className="experience-content">
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>NodeJS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>ExpressJS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>MongoDB</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>Python</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>PHP</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience-details">
+              <BsFillPatchCheckFill className='experience-details-icon' />
+              <div>
+              <h4>MySQL</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
